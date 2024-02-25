e853cef719950fe83118f8bc8b4fc625573c701c
amardeep <amardeepjk@gmail.com>
2024-02-25 19:20:42 +0800

    hhh

--- tracers/amar.py
+++ tracers/amar.py
@@ -1,6 +1,6 @@
-bad_words = ['std::', '__gnu_cxx', '__gthread_active_p', "QuantLib::Date", "QuantLib::Calendar"]
+bad_words = ['/usr/include', "/usr/lib/gcc"]
 
-with open('../cmake-build-debug/Examples/CVAIRS/amar1.txt') as oldfile, open('final.txt', 'w') as newfile:
+with open('includefull.txt') as oldfile, open('include.txt', 'w') as newfile:
     for line in oldfile:
         if not any(bad_word in line for bad_word in bad_words):
             newfile.write(line)
